35753daa9126096f264e0e04ae33e4694ad58938
RafaellJ1979 <rafal1979jaskowski@gmail.com>
2021-03-31 16:12:51 +0200

    zadanie JS : zmienne Lekcja 8

---
+++ lesson 8/zadanie Js zmienne/zadanie.js
@@ -0,0 +1,23 @@
+//
+
+let firstName = "Adam";
+console.log(firstName);
+
+let age = 50;
+console.log(age);
+
+let isOpen = true;
+console.log(isOpen);
+
+// 
+
+firstName = "Jan";
+console.log(firstName);
+
+age = 30;
+console.log(age);
+
+isOpen = false;
+console.log(isOpen);
+
+
