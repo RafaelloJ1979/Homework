4a81f933ab4fd158533fb362687da195d61bd3e4
RafaellJ1979 <rafal1979jaskowski@gmail.com>
2021-04-02 13:55:15 +0200

    Lesson 9 zadania js

---
+++ lesson 9/javascript.js
@@ -0,0 +1,52 @@
+function power(a) {
+  console.log(a ** a);
+}
+power(3);
+
+function multiple(x, y) {
+  console.log(x * y);
+}
+multiple(2, 3);
+
+function changeMinutesForSeconds(s) {
+  let minute = 60;
+  console.log(minute * s);
+}
+changeMinutesForSeconds(60);
+
+function sayHello(name) {
+  console.log("Cześć " + name);
+}
+sayHello("Ilona");
+
+//
+
+function sayHello1(a, b) {
+  console.log(a + b);
+}
+sayHello1(2, 2);
+
+function sayHello2(a, b) {
+  console.log(a - b);
+}
+sayHello2(4, 2);
+
+function sayHello3(a, b) {
+  console.log(a / b);
+}
+sayHello3(8, 2);
+
+function sayHello4(a, b) {
+  console.log(a * b);
+}
+sayHello4(2, 4);
+
+function sayHello5(a, b) {
+  console.log(a % b);
+}
+sayHello5(8, 3);
+
+function sayHello6(a, b) {
+  console.log(a ** b);
+}
+sayHello6(2, 3);
