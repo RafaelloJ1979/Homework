e53e57d3951c67a39706c53546c4b366ae44bdff
RafaellJ1979 <rafal1979jaskowski@gmail.com>
2021-04-08 00:29:05 +0200

    funkcje i zmienne JS praca domowa Lesson 9

---
+++ lesson 9/jsTasks.js
@@ -0,0 +1,225 @@
+/* zad.1: */
+let name = "Karol";
+let age = 40;
+let isOpen = true;
+
+/* zad.2: */
+console.log("Michał");
+console.log("Hello Borys");
+console.log(4);
+console.log(false);
+
+/* zad.2 do 1  */
+console.log(name);
+console.log(age);
+console.log(isOpen);
+
+/* zad.3: */
+console.log(2 + 2);
+console.log("Micha " + "Kowalski");
+console.log(2 + 2 * 2);
+
+/* zad.4: */
+let number = 2 + 2;
+console.log(number);
+
+/* zad.5: */
+let num1 = 1;
+num1 = 2;
+num1 = 3;
+console.log(num1);
+
+/* zad.6: */
+/* a */
+function sum() {
+  let result = 2 + 2;
+  console.log(result);
+  return result;
+}
+sum();
+
+/* b */
+function text() {
+  let result = "Hello World";
+  console.log(result);
+  return result;
+}
+text();
+
+/* c */
+function boolean() {
+  let result = true;
+  console.log(result);
+  return result;
+}
+boolean();
+
+/* d */
+function division() {
+  let result = 6 % 4;
+  console.log(result);
+  return result;
+}
+division();
+
+/* e */
+function exponentiation() {
+  let result = 3 ** 4;
+  console.log(result);
+  return result;
+}
+exponentiation();
+
+/* f */
+function stringReturn() {
+  let result = "Hello, ";
+  console.log(result + "how are you ?");
+  return result;
+}
+stringReturn();
+
+/* zad: 7 */
+/* a */
+function sum(a, b) {
+  console.log(a + b);
+  return a + b;
+}
+sum(2, 2);
+
+/* b */
+function text(txt1, txt2) {
+  console.log(txt1 + txt2);
+  return txt1 + txt2;
+}
+text("Hello ", "World");
+
+/* c */
+function boolean(Bool) {
+  console.log(Bool);
+  return Bool;
+}
+boolean(true);
+
+/* d */
+odp: function division() {
+  let result = 6 % 4;
+  console.log(result);
+  return result;
+}
+division();
+/* e */
+function exponentiation() {
+  let result = 3 ** 4;
+  console.log(result);
+  return result;
+}
+exponentiation();
+/* f */
+function stringReturn() {
+  let result = "Hello, ";
+  console.log(result + "how are you ?");
+  return result;
+}
+stringReturn();
+/* zad: 7 */
+/* a */
+function sum(a, b) {
+  console.log(a + b);
+  return a + b;
+}
+sum(2, 2);
+/* b */
+function text(txt1, txt2) {
+  console.log(txt1 + txt2);
+  return txt1 + txt2;
+}
+text("Hello ", "World");
+/* c */
+function boolean(Bool) {
+  console.log(Bool);
+  return Bool;
+}
+boolean(true);
+
+/* d */
+function division(div1, div2) {
+  console.log(div1 % div2);
+  return div1 % div2;
+}
+division(6, 4);
+
+/* e */
+function exponentiation(n1, n2) {
+  console.log(n1 ** n2);
+  return n1 ** n2;
+}
+exponentiation(3, 4);
+
+/* f */
+function stringReturn(textOne, textTwo) {
+  console.log(textOne + textTwo);
+  return textOne + textTwo;
+}
+stringReturn("Praca ", "Domowa");
+
+/* g */
+function time(min) {
+  console.log(min * 60);
+  return min * 60;
+}
+time(3);
+
+/* h */
+function timeHour(hour) {
+  console.log(hour * 60 * 60);
+  return hour * 60 * 60;
+}
+timeHour(1);
+
+/* i */
+function triArea(a, b) {
+  console.log(a * b * 0.5);
+  return a * b * 0.5;
+}
+triArea(5, 5);
+
+/* j */
+function years(year) {
+  console.log(year * 365);
+  return year * 365;
+}
+years(5);
+
+/* k */
+function power(num1) {
+  console.log(num1 ** num1);
+  return num1 ** num1;
+}
+power(4);
+
+/* L */
+function TriAreaAdd(side1, side2) {
+  console.log(side1 + side2 - 1);
+  return side1 + side2 - 1;
+}
+TriAreaAdd(5, 5);
+
+/* m */
+function seyHelloName(firstName) {
+  console.log("Cześć " + firstName);
+  return "Cześć " + firstName;
+}
+seyHelloName("Iza");
+
+/* n */
+function yourAge(age) {
+  console.log(2021 - age);
+  return 2021 - age;
+}
+yourAge(1979);
+
+/* o */
+function basket(two, three) {
+  console.log(two * 2 + three * 3);
+}
+basket(3, 3);
