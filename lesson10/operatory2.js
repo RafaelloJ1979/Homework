549baa104558109f2af91abd5aefb45a9dad3189
Rafal Jaskowski <rafal1979jaskowski@gmail.com>
2021-04-16 17:16:53 +0200

    live10-taskBart

--- lesson10/operatory2.js
+++ lesson10/operatory2.js
@@ -44,11 +44,14 @@ const suszarka = ["spodnie", "majty", "koszulka", "koszulka"];
 const brudneUbrania = ["brudne majty", "brudna koszulka", "brudne spodnie"];
 
 function zrobPranie(ubrania, proszek, plyn, program = "sport") {
-  console.log("proszek przed zmiana", proszek);
-  if (proszek == "proszek czarnobialy") {
-    proszek = undefined;
+  console.log("brudneUbrania przed zmiana", ubrania);
+
+  if (ubrania.length < 5) {
+    ubrania.push("brudne skarpety");
+    ubrania.push("brudne biale skarpety");
   }
-  console.log("proszek po zmiana", proszek);
+
+  console.log("brudneUbrania po zmiana", brudneUbrania);
 
   // console.log("ubrania", ubrania);
   // console.log("proszek", proszek);
@@ -80,14 +83,57 @@ function zrobPranie(ubrania, proszek, plyn, program = "sport") {
 
 // const proszek = srodkiMyjace[0]
 
-const proszeczek = "proszek czarnobialy";
-console.log("proszeczek przed", proszeczek);
-const koszNaPranie = zrobPranie(
-  brudneUbrania,
-  proszeczek,
-  srodkiMyjace[1],
-  "delikatne"
-);
-console.log("proszeczek po", proszeczek);
+// const proszeczek = "proszek czarnobialy";
+
+// console.log("brudne Ubrania p rzed", brudneUbrania);
+
+// const koszNaPranie = zrobPranie(brudneUbrania,'Persil',srodkiMyjace[1],"delikatne");
+// console.log("brudne Ubrania po", brudneUbrania);
 
 // console.log(koszNaPranie);
+
+// let a = [1, 2, 3];
+// let b = a;
+// przypiszemy A , tylko przypisuje B wartośc A a wartosc A jest adresem tej tablicy [123]
+// a = [2, 2, 2];
+
+// console.log(a);
+// 222
+// console.log(b);
+// 123 to co bylo w a -- B przypsiało adres tablicy 123
+
+// kopia zmiennej złozonej np tablicy
+// np  const ubraniaKopia = [...ubrania]    czyli   ...ubrania
+
+// const ubraniaKopia = [...ubrania[1,2,3]]
+// console.log(...ubrania)
+
+// kazda zmienna dostała kopie aaa
+// let a = 'aaa'
+// let b = a
+// let c = b
+// a = 'nic'
+// console.log('a',a)
+// console.log('b',b)
+// console.log('c',c)
+
+// a w typach złożponych np tablice obiekty kazda tablica uzyska ta sama wartosc
+// let a = [1,2,3] // adres jakis tam 0x23434f23
+// let b = a // 0x23434f23
+// let c = b // 0x23434f23
+
+// a.push(4, 5, 6)
+// console.log('a',a)
+// console.log('b',b)
+// console.log('c',c)
+
+let a = [1, 2, 3]; // adres np 4x334e4f55
+let b = a; // 0x23434f23
+let c = b; // 0x23434f23
+
+a = ["Mateusz"]; // nowy adres 4x334e4f55
+c.push(4, 5, 6);
+
+console.log("a", a);
+console.log("b", b);
+console.log("c", c);
