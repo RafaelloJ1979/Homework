e07c7fe413c41d71288fb4a89770606e35325261
Rafal Jaskowski <rafal1979jaskowski@gmail.com>
2021-05-02 04:12:07 +0200

    Lesson15 Praca Domowa

--- lesson15/lesson15PracaDomowa.js
+++ lesson15/lesson15PracaDomowa.js
@@ -1,72 +1,77 @@
-// zad 1
 
-// a) wyświetli wartości od 1 do 9,
+// 1 a)
 console.log('\n', 'Z A D A N I E : 1 a', '\n')
 
 for (let i = 0; i < 10; i++) {
     console.log("wartość", i)
 }
-// b) wyświetli wartości od 9 do 1,
+
+// 1 b) 
 console.log('\n', 'Z A D A N I E : 1 b', '\n')
 
 for (let i = 9; i > 0; i--) {
     console.log("wartość", i)
 }
-// c) wyświetli wartości od 5 do 15,
+
+// 1 c) 
 console.log('\n', 'Z A D A N I E : 1 c', '\n')
 
 for (let i = 5; i <= 15; i++) {
     console.log("wartość", i)
 }
-// d) wyświetli wartości od 0 do -10,
+
+// 1 d) 
 console.log('\n', 'Z A D A N I E : 1 d', '\n')
 
 for (let i = 0; i >= -10; i--) {
     console.log("wartość", i)
 }
-// e) wyświetli wartości od 1 do 20 zwiększając się co 2 w każdej iteracji,
+
+// 1 e) 
 console.log('\n', 'Z A D A N I E : 1 e', '\n')
 
 for (let i = 1; i <= 20; i += 2) {
     console.log("wartość", i)
 }
-// f) wyświetli wartości od 10 do -10 zmniejszając się o 4 w każdej iteracji,
+
+// 1 f) 
 console.log('\n', 'Z A D A N I E : 1 f', '\n')
 
 for (let i = 10; i >= -10; i -= 4) {
     console.log("wartość", i)
 }
-// g) wyświetli wartości od 1 do 100 zwiększając liczbę co iterację mnożąc ją razy 2,
+
+// 1 g) 
 console.log('\n', 'Z A D A N I E : 1 g', '\n')
 
 for (let i = 1; i < 100; i *= 2) {
     console.log("wartość", i)
 }
-// zad 2
 
-// a) wyświetli wartości od 0 do 20, ale tylko liczby parzyste,
+// 2 a) 
 console.log('\n', 'Z A D A N I E : 2 a', '\n')
 
 for (let i = 0; i <= 20; i++) {
     if (i % 2 === 0)
         console.log("wartość", i)
 }
-// b) wyświetli wartości od 0 do 20, ale tylko liczby nieparzyste,
+
+// 2 b) 
 console.log('\n', 'Z A D A N I E : 2 b', '\n')
 
 for (let i = 0; i <= 20; i++) {
     if (i % 2 !== 0)
         console.log("wartość", i)
 }
-// c) wyświetli wartości od 0 do 10, ale tam gdzie jest liczba parzystą pokaże napis liczba parzysta, a tam gdzie nie niech wyświetli napis liczba nieparzysta,
+
+// 2 c) 
 console.log('\n', 'Z A D A N I E : 2 c', '\n')
 
 for (let i = 0; i <= 20; i++) {
     console.log(i % 2 === 0 ? `parzysta ${i}` : `nieparzysta ${i}`)
 }
-// d) wyświetli liczby od 1 do 30 i tam gdzie liczba jest podzielna tylko przez 3 niech pokaze Fizz, tam gdzie przez 5 niech
-// pokaże Buzz, a tam gdzie przez 3 i 5 niech wyświetli FizzBuzz, w reszcie przypadków niech pokaże po prostu cyfry,
-// obok niech napis "- podzielna przez 10",
+
+// 2 d) 
 console.log('\n', 'Z A D A N I E : 2 d', '\n')
 
 for (i = 0; i <= 30; i++) {
@@ -80,8 +85,8 @@ for (i = 0; i <= 30; i++) {
         console.log(i)
     }
 }
-// e) wyświetli liczby od 100 do 0, ale zmniejszając się o 5 co iteracje i jak liczba jest podzielna przez 10 to niech doda
-// obok niech napis "- podzielna przez 10",
+
+// 2 e) 
 console.log('\n', 'Z A D A N I E : 2 e', '\n')
 
 for (i = 100; i >= 0; i -= 5) {
@@ -91,9 +96,8 @@ for (i = 100; i >= 0; i -= 5) {
         console.log(i)
     }
 }
-// // zad 3 Napisz funkcje, która:
 
-// a) przyjmuje jako parametr liczbe do której wykona się pętla od 0,
+// 3 a) 
 console.log('\n', 'Z A D A N I E : 3 a', '\n')
 
 function startLoop(startNumber) {
@@ -102,7 +106,7 @@ function startLoop(startNumber) {
     }
 }
 startLoop(10)
-// b) przyjmuje jako parametr liczbe od której pętla się wykona do 15,
+// 3 b) 
 console.log('\n', 'Z A D A N I E : 3 b', '\n')
 
 function strartLoop15(startNumber) {
@@ -110,7 +114,7 @@ function strartLoop15(startNumber) {
         console.log(i)
 }
 strartLoop15(12)
-// c) przyjmuje jako parametr liczbę wskazującą o ile ma się zwiększać pętla co iteracje i wyświetla taką pętle od 0 do 15,
+// 3 c)
 console.log('\n', 'Z A D A N I E : 3 c', '\n')
 
 function startLoopHowMuchToIncrase(increase) {
@@ -119,7 +123,7 @@ function startLoopHowMuchToIncrase(increase) {
 }
 startLoopHowMuchToIncrase(5)
 
-// d) przyjmuje dwa parametry, liczbę od której ma się pętla rozpocząć i do jakiej ma trwać,
+// 3 d) 
 console.log('\n', 'Z A D A N I E : 3 d', '\n')
 
 function startLoopStopLoop(start, stop) {
@@ -127,8 +131,7 @@ function startLoopStopLoop(start, stop) {
         console.log(i)
 }
 startLoopStopLoop(2, 5)
-// e) przyjmuje trzy argumenty, pierwszy - liczba od której zaczynamy pętle, drugi - liczbę do której pętla ma trwać, trzeci -
-//     liczbę co ile ma się pętla zwiększać,
+// 3 e) 
 console.log('\n', 'Z A D A N I E : 3 e', '\n')
 
 function startStopIncreaseLoop(start, stop, increase) {
@@ -138,9 +141,7 @@ function startStopIncreaseLoop(start, stop, increase) {
 }
 startStopIncreaseLoop(1, 10, 2)
 
-//zad 4  Napisz funkcje, która:
-
-// a) stwórz tablice 5 elementową i za pomocą pętli wypisz wszystkie jej elementy,
+// 4 a) 
 console.log('\n', 'Z A D A N I E : 4 a', '\n')
 
 const tabFive = [1, "dwa", 3, "cztery", 5]
@@ -152,8 +153,7 @@ function tabWrite() {
 }
 tabWrite()
 
-// b) stwórz tablice z kilkoma imionami i wyświetl "Cześć {imie}"
-// za pomocą pętli dla każdego imienia,
+// 4 b) 
 console.log('\n', 'Z A D A N I E : 4 b', '\n')
 
 const tabName = ["Adam", "Ewa", "Jacek", "Agata"]
@@ -166,17 +166,7 @@ function displayString() {
 }
 displayString()
 
-// lub?
-// const tabName = ["Adam", "Ewa", "Jacek", "Agata"]
-
-// function displayString() {
-//     for (let i = 0; i < tabName.length; i++) {
-//         console.log(` Cześć ${tabName[i]} `)
-//     }
-// }
-// displayString()
-
-// c) stwórz tablicę z obiektami, w każdym obiekcie niech będzie model auta i kolor, i wyświetl w stringu "Przed Wami stoi {kolor} {model}",
+// 4 c) 
 console.log('\n', 'Z A D A N I E : 4 c', '\n')
 
 const sportsCars = [
@@ -205,7 +195,7 @@ function giveMeCar(view) {
 }
 giveMeCar(sportsCars)
 
-// d) stwórz tablicę z 10 elementami, czyli cyframi od 1 do 10 i niech wyświetli ona tylko liczby parzyste z tej tablicy
+// 4 d) 
 console.log('\n', 'Z A D A N I E : 4 d', '\n')
 
 const tabNumberTen = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -219,7 +209,7 @@ function evenNumber() {
 }
 evenNumber()
 
-// e) ** stwórz tablice kilku elementową o różnych typach i wyświetl z tablicy tylko te elementy które są stringami,
+// 4 e) ** 
 console.log('\n', 'Z A D A N I E : 4 e', '\n')
 
 const mixTable = ["Jakiś tekst", 5, "Adam", 10, "Warszawa", 7, ];
@@ -233,7 +223,7 @@ function checkTable(stringDisplay) {
 }
 checkTable(mixTable);
 
-// f) ** stwórz tablice kilku elementową o różnych typach i wrzuć wszystkie elementy które w tej tablicy są numberami do nowej tablicy,
+// 4 f) ** 
 console.log('\n', 'Z A D A N I E : 4 f', '\n')
 
 const mixTable2 = ["Jakiś tekst", 5, "Adam", 10, "Warszawa", 15];
@@ -249,9 +239,7 @@ function checkTableTwo(stringDisplay) {
 }
 checkTableTwo(mixTable2)
 
-// zad 5    // Napisz funkcję, która:  
-
-// a) przyjmuje tablicę liczb jako parametr i liczy sume elementów tej tablicy,
+// 5 a) 
 console.log('\n', 'Z A D A N I E : 5 a', '\n')
 
 const tabFirst = [1, 2, 3, 4, 5]
@@ -261,7 +249,7 @@ function sumArray(addUp) {
 }
 sumArray(tabFirst)
 
-// b) przyjmuje tablicę z numberami i z tego tworzy dwie nowe tablice z liczbami parzystymi i nieparzystymi,
+// 5 b) 
 console.log('\n', 'Z A D A N I E : 5 b', '\n')
 
 const tabNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
@@ -282,8 +270,7 @@ function evenAndOdd(evenOdd) {
 }
 evenAndOdd(tabNumber)
 
-// c) przyjmuje string jako parametr i jeśli string zawiera w sobie "y"
-// niech wyświetli Jest!, a w inny przypadku Nie ma!
+// 5 c) 
 console.log('\n', 'Z A D A N I E : 5 c', '\n')
 
 let text = "yes yes"
@@ -298,10 +285,7 @@ function giveMeString(string) {
 }
 giveMeString(text)
 
-// d) przyjmuje liczbę jako parametr i wyświetla wynik mnożenia jej razy cyfry pomniejszone o 1 aż do 1 ego, np.
-// countResult(4) // 4 * 3 * 2 * 1 = 24
-// // Przykładowy wynik
-// // 24
+// 5 d) 
 console.log('\n', 'Z A D A N I E : 5 d', '\n')
 
 function reduce(num) {
@@ -312,13 +296,7 @@ function reduce(num) {
 }
 reduce(5);
 
-// e) Przyjmuje tablicę jako jeden parametr i string "number"
-// lub "string"
-// jako drugi parametr, w zależności od drugiego parametru niech funkcja
-// dodaje do siebie elementu number lub string do siebie tworząc sumę lub jeden długi string, jeśli drugi parametr nie będzie sie zgadzał z "number"
-//     lub "string"
-// to
-// niech pokaże odpowiedni komunikat "Drugi parametr jest nieprawidłowy!", dodaj jeszcze zabezpieczenie jeśli tablica będzie pusta
+// 5 e) 
 console.log('\n', 'Z A D A N I E : 5 e', '\n')
 
 const arrayOne = [10, "555"]
@@ -334,9 +312,7 @@ function takesTheNumberAndTheText(tab = 1, string) {
 }
 takesTheNumberAndTheText(arrayOne, 55)
 
-// f) przyjmuje jako parametr tablicę obiektów.W każdym obiekcie powinno znajdować się: name, lastName, hasCar, hasDrivingLicence.Niech będzie
-// to tablica z 4 osobami.Stwórz funkcję które przyjmie taką tablicę jako parametr i sprawdzi nam kto ma prawo jazdy i samochód i kto może Cię
-// zabrać do domu z imprezy, niech również wyświetli odpowiedni komunikat jeśli ktoś ma prawko ale nie ma auta lub nie ma ani jednego ani drugiego,
+// 5 f) 
 console.log('\n', 'Z A D A N I E : 5 f', '\n')
 
 const footballers = [
@@ -380,8 +356,7 @@ function showMeWho(view) {
 }
 showMeWho(footballers)
 
-// g) przyjmuje jako parametr tablice i nowy element do tablicy.Funkcja ta powinna przeiterować tablice sprawdzając czy już taki element w niej jest
-// jeśli go nie ma to dodać do tablicy, a jak jest to wyświetlić odpowiedni komunikat,
+// 5 g) 
 console.log('\n', 'Z A D A N I E : 5 g', '\n')
 
 let tableString = ["one", "two", "three", "four", "five"]
