c871e20c3a69294f91af9cc5a6d5a6904c959ebc
Rafal Jaskowski <rafal1979jaskowski@gmail.com>
2021-05-06 20:15:05 +0200

    L11 poprawione zadanie z liva

--- lesson11/lesson11.js
+++ lesson11/lesson11.js
@@ -1,18 +1,37 @@
 const person = {
-    age: 30,
-    isDrunk: true,
-    name: "Kasia",
+  age: 15,
+  isDrunk: true
+}
+
+function canServeBeer(age, isDrunk) {
+  let wiek = age.age
+  if (wiek >= 18 && isDrunk === false) {
+    console.log(`Możesz kupić piwko , ponieważ masz ${wiek} lat i jesteś trzeźwy`)
+    /* console.log("mozesz kupic piwko bo masz lat",wiek) */
+  } else {
+    console.log(`nie możesz kupic piwka ponieważ masz ${wiek} lat`)
   }
-  
-  function canServBeer(osoba) {
-  
-    if (osoba >= 18) {
-      console.log(person.name + " oto Twoje winko")
-    } else if (osoba < 18) {
-      console.log(person.name + " , przykro mi, dzisiaj nie ma winka")
-    }
-    return person
-  
+
+}
+
+canServe = canServeBeer(person, false)
+
+// przykład z autem i przekazaniem do funkcji obiektu
+const pustaTablica = []
+const samochod = {
+  model: "ford",
+  type: "Mustang",
+  color: "red"
+}
+
+function noweAuto(car, podaj) {
+  if (car && car.type) {
+    podaj.push(car)
+    console.log(pustaTablica)
   }
-   const osoba = canServBeer(1)
-  
+}
+console.log('before', pustaTablica)
+noweAuto(samochod, pustaTablica)
+console.log('after', pustaTablica)
+
+//
