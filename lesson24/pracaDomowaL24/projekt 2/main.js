ce4a047379e91fcbe7f37fdece4858dd9c1d8584
Rafaello <rafal1979jaskowski@gmail.com>
2021-06-09 20:32:07 +0200

    korekta w zadaniu projekt 2

--- lesson24/pracaDomowaL24/projekt 2/main.js
+++ lesson24/pracaDomowaL24/projekt 2/main.js
@@ -1,79 +1,4 @@
-// function Promise2() {
-//     this.then = () => {
-//         console.log('then')
-//     }
-// }
-// const fakePromise = new Promise2()
-// fakePromise.then()
-
-// mozemy wyniesc po za fn zeby miec dostep  w then
-
-// const imieDiv = document.getElementById('imie')  
-
-// function pobierzImie(index) {
-//     const imiona = ['Mateusz','Kasia','Agnieszka','Łukasz','Grzesiek','Piotrek']
-//     const imieDiv = document.getElementById('imie')
-//     imieDiv.innerHTML = 'Loading'
-//     return new Promise((resolve) => {
-//         setTimeout(() => resolve('Mateusz'), 2000)
-//     })
-// }
-// const promise = pobierzImie()
-// promise.then((response) => {
-//     const imieDiv = document.getElementById('imie')
-//     imieDiv.innerHTML = response
-// })
-
-// --------------------------------
-
-// async function main() {
-//     function pobierzImie(index, czas = 1000) {
-//         const imiona = ['Mateusz', 'Kasia', 'Agnieszka', 'Łukasz', 'Grzesiek', 'Piotrek']
-//         return new Promise((resolve) => {
-//             setTimeout(() => resolve(imiona[index]), czas)
-//         })
-//     }
-
-// ponizsze promisy wykonaja sie jak ostatni zostanie zwrocony
-
-//     const promise1 = pobierzImie(2)
-//     const promise2 = pobierzImie(4)
-//     const promise3 = pobierzImie(5,4000)
-//     Promise.all([promise1, promise2, promise3]).then(Response => console.log(Response))
-
-// n/w wykonaja sie niezaleznie a po wykonaniu ostatniego dostaniemy promise ALL
-// then odkłada na bok , pomija kod i wykonuje poerwsze w kolejnosci.
-// w pdzypadku promisow kod wykonuje sie asynchro poniweaz then odkłada do kolejki na póxniej
-
-//     const promise1 = pobierzImie(2,3000)
-//     promise1.then(response => {
-//         console.log('imie 1',response)
-//     })
-//     const promise2 = pobierzImie(4,1000)
-//     promise2.then(response => {
-//         console.log('imie 2',response)
-//     })
-//     const promise3 = pobierzImie(5, 2000)
-//     promise3.then(response => {
-//         console.log('imie 3',response)
-//     })
-//     Promise.all([promise1, promise2, promise3]).then(Response => console.log(Response))
-
-// n/w kod async await wykonuje sie linijka po linijce , poniewz jest to kod asynchroniczny pisay w sposob synchroniczny // AWAIT czeka ,blokuje az wykona sie dane z np pobierzImie
-
-//     const imie1 = await pobierzImie(2)
-//     console.log('imie1', imie1)
-
-//     const imie2 = await pobierzImie(4)
-//     console.log('imie2', imie2)
-
-//     const imie3 = await pobierzImie(5)
-//     console.log('imie3', imie3)
-
-// }
-// main()
-
-// **********************   PROJEKT L24 ******************** //
+// **********************   PROJEKT 2 ******************** //
 
 let page = 1;
 let info = null;
@@ -97,9 +22,6 @@ async function main() {
         const $listaPostaci = document.getElementById('lista')
         $listaPostaci.innerHTML = ''    // czyszczenie listy
         characters.results.forEach(stworzKartePostaci)
-        // pobierzPostaci()
-        // console.log('fn', pobierzPostaci())      wywoływanie fn podczas kliku
-        // console.log('after info', info)
         // el.target.innerHTML = 'kliknołeś' // zmiana teksty na buttonie
     })
 
@@ -115,10 +37,6 @@ async function main() {
         const $listaPostaci = document.getElementById('lista')
         $listaPostaci.innerHTML = ''    // czyszczenie listy
         characters.results.forEach(stworzKartePostaci)
-        // pobierzPostaci()
-        // console.log('fn', pobierzPostaci())      wywoływanie fn podczas kliku
-        // console.log('after info', info)
-        // el.target.innerHTML = 'kliknołeś' // zmiana teksty na buttonie
     })
 
     async function pobierzPostaci() {
@@ -157,11 +75,7 @@ async function main() {
         $lista.appendChild($card)
     }
 
-    // stworzKartePostaci()
-
     const characters = await pobierzPostaci()
-
-    // wrzucamy iformacje do zmiennej INFO
 
     info = characters.info
 
@@ -177,25 +91,8 @@ async function main() {
 
     console.log('char', characters)
 
-    // characters.results.forEach((characters)=>stworzKartePostaci(characters))
-    // albo krócej
-
     characters.results.forEach(stworzKartePostaci)
 
 }
 main()
 
-/* <div class="card">
-<img src="img_avatar.png" alt="Avatar" style="width:100%">
-<div class="container">
-<h4><b>John Doe</b></h4>
-<p>Architect & Engineer</p>
-</div>
-</div> */
-
-    // episode: (41) ["https://rickandmortyapi.com/api/episode/1", "https://rickandmortyapi.com/api/episode/2", "https://rickandmortyapi.com/api/episode/3", "https://rickandmortyapi.com/api/
-    // gender: "Male"
-    // id: 1
-    // image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg"
-    // location: {name: "Earth (Replacement Dimension)", url: "https://rickandmortyapi.com/api/location/20"}
-    // name: "Rick Sanchez"
