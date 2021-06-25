224ad83548bd3ca16fc2ac930658f6a1bd9aa04c
Rafaello <rafal1979jaskowski@gmail.com>
2021-06-25 17:43:38 +0200

    call

--- testowy/call.js
+++ testowy/call.js
@@ -1,58 +1,58 @@
-// const human = {
-//     name: 'Tom',
-//     sayName() {
-//         console.log(`My name is ${this.name}`)
-//     },
-//     sayFullName: function (surname, age) {
-//         console.log(`My name is ${this.name} ${surname}`)
-//     }
-// }
-// // metoda call
+const human = {
+    name: 'Tom',
+    sayName() {
+        console.log(`My name is ${this.name}`)
+    },
+    sayFullName: function (surname, age) {
+        console.log(`My name is ${this.name} ${surname}`)
+    }
+}
+// metoda call
 
-// const personA = {
-//     name: 'Artur'
-// }
-// const personB = {
-//     name: 'Tom'
-// }
-// human.sayName.call(personB)
-// const people = [
+const personA = {
+    name: 'Artur'
+}
+const personB = {
+    name: 'Tom'
+}
+human.sayName.call(personB)
+const people = [
 
-//     {
-//         name: 'Artur'
-//     },
-//     {
-//         name: 'Tom'
-//     }
-// ]
+    {
+        name: 'Artur'
+    },
+    {
+        name: 'Tom'
+    }
+]
 
-// people.forEach(p => {
-//     human.sayName.call(p)
-// })
+people.forEach(p => {
+    human.sayName.call(p)
+})
 
-// // metoda apply
+// metoda apply
 
-// people.forEach(p => {
-//     human.sayFullName.apply(p, ['surname', 55])
-// })
+people.forEach(p => {
+    human.sayFullName.apply(p, ['surname', 55])
+})
 
-// const human2 = {
-//     /*  name: 'Tom', */
-//     sayName() {
-//         console.log(`My name is ${this.name}`)
-//     },
-//     sayFullName: function (surname, age) {
-//         console.log(`My name is ${this.name} ${surname} lat ${age}`)
-//     }
-// }
-// const person = {
-//     name: 'ADam',
-//     age: 55,
-//     surname: 'Dec'
-// }
+const human2 = {
+    /*  name: 'Tom', */
+    sayName() {
+        console.log(`My name is ${this.name}`)
+    },
+    sayFullName: function (surname, age) {
+        console.log(`My name is ${this.name} ${surname} lat ${age}`)
+    }
+}
+const person = {
+    name: 'ADam',
+    age: 55,
+    surname: 'Dec'
+}
 
-// people.forEach(p => {
-//     human2.sayFullName.apply(p, ['Kowalski', 55])
-// })
+people.forEach(p => {
+    human2.sayFullName.apply(p, ['Kowalski', 55])
+})
 
 
